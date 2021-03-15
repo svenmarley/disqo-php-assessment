79aed7c2893105e3365ff8b17039c9ee3732d612
Mike Anderson <mikea@candicontrols.com>
2021-03-15 11:51:39 -0700

    Features: Tests and Logging
    Implemented logging using the Laravel Monolog class.
    
    Implemented testing using the Laravel phpunit implementation.
    
    Built a simple Notes testing section - can be expanded later.

---
+++ tests/Feature/NoteTest.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Http\Response;
+
+
+class NoteTest extends TestCase
+{
+    private $sFunc = 'TestCase';
+
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function test_example()
+    {
+        $response = $this->get('/');
+
+        $response->assertStatus(200);
+    }
+
+    public function test_note_attributes()
+    {
+        $debug = false;
+        $sFunc = $this->sFunc . '.test_note_attributes()-->';
+        $headers = [
+            'Accept' => 'application/vnd.api+json, application/json',
+            'Content-Type' => '',
+
+            // apparently, doesn't need these to work
+                //'Accept-Encoding' => 'gzip, deflate, br, *',
+                //'Accept-Charset' => '*',
+                //'Accept-Language' => '*',
+        ];
+
+        $response = $this->json('get', '/api/v1/notes', [], $headers);
+
+        if ($debug) {
+            Log::info($sFunc . 'response');
+            $j = $response->json();
+            Log::info($sFunc . 'j=' . print_r($j, true));
+        }
+
+        $response->assertStatus(Response::HTTP_OK)
+            ->assertJsonStructure([
+                'data' => [
+                    '*' => [
+                        'attributes' => [
+                            'Title',
+                            'Note'
+                        ]
+                    ]
+                ]
+            ]);
+    }
+}
